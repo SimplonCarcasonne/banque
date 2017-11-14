d9ead602014726c47b6b4295187d74caba786ef8
Pierre-François de Soos <pfdesoos@simplon.co>
2017-11-14 09:05:03 +0100

    maj

--- class/PersonneManager.php
+++ class/PersonneManager.php
@@ -5,6 +5,10 @@ class PersonneManager
   private $personnes = [];
 
   //Singleton ( La classe ne peut être instanciée qu'une fois)
+  //Pour cela on crée une propriété de classe ( static ) qui contiendra l'instance unique
+  //On donne accès à cette propriete par un getter de classe: getInstance
+  //On bloque la possibilité de faire un new en rendant __construct privé
+  //On bloque la possibilité de cloner l'objet en rendant __clone privé
   private static $instance;
 
 
